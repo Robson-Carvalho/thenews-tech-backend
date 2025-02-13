2c74538fdc7d76add6def7c4baa8fb2a22269ab5
robson-carvalho <robsoncarvalho.tech@gmail.com>
2025-02-13 14:32:41 -0300

    feat: melhora arquivo de configurção de env e remove arquivos de testes

--- src/infrastructure/config/env/index.ts
+++ src/infrastructure/config/env/index.ts
@@ -2,20 +2,17 @@ import dotenv from "dotenv";
 
 import path from "path";
 
-if (process.env.NODE_ENV !== "production") {
-  const envPath =
-    process.env.NODE_ENV === "prod"
-      ? path.resolve(__dirname, "../../../../.env.production.local")
-      : process.env.NODE_ENV === "develop"
-      ? path.resolve(__dirname, "../../../../.env.development.local")
-      : path.resolve(__dirname, "../../../../.env.test.local");
+if (process.env.NODE_ENV === "prod") {
+  dotenv.config({ path: path.resolve(__dirname, "../../../../.env.prod") });
+} else if (process.env.NODE_ENV === "prod") {
+  dotenv.config({ path: path.resolve(__dirname, "../../../../.env.dev") });
+} else if (process.env.NODE_ENV === "prod") {
+  dotenv.config({ path: path.resolve(__dirname, "../../../../.env.test") });
+}
 
-  dotenv.config({ path: envPath });
-
-  if (!process.env.DATABASE_URL) {
-    console.error(
-      "Error: DATABASE_URL is not defined in the environment variables."
-    );
-    process.exit(1);
-  }
+if (!process.env.DATABASE_URL) {
+  console.error(
+    "Error: DATABASE_URL is not defined in the environment variables."
+  );
+  process.exit(1);
 }
